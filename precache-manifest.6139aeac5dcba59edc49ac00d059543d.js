303e6ad9afccde0502c79fc5723553933d89f6c5
ghspbravo <gmspbravo@gmail.com>
2019-05-17 23:59:21 +0500

    Updates

---
+++ precache-manifest.6139aeac5dcba59edc49ac00d059543d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96991d635f3ae85f2dab44d636f1df4",
+    "url": "/koriphey/index.html"
+  },
+  {
+    "revision": "db8dbdc876f1d4ba9263",
+    "url": "/koriphey/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6a983a26edf542e9d4a5",
+    "url": "/koriphey/static/css/main.654e7ff3.chunk.css"
+  },
+  {
+    "revision": "db8dbdc876f1d4ba9263",
+    "url": "/koriphey/static/js/2.cb03f833.chunk.js"
+  },
+  {
+    "revision": "6a983a26edf542e9d4a5",
+    "url": "/koriphey/static/js/main.8cd1b6fa.chunk.js"
+  },
+  {
+    "revision": "f768937c358599e3d953",
+    "url": "/koriphey/static/js/runtime~main.4c9e406a.js"
+  },
+  {
+    "revision": "1f529a4fe05d531ec5701e153972c5f8",
+    "url": "/koriphey/static/media/DroidSans-Bold.1f529a4f.woff"
+  },
+  {
+    "revision": "4193e18279deda75596b4947b50af3d8",
+    "url": "/koriphey/static/media/DroidSans-Bold.4193e182.ttf"
+  },
+  {
+    "revision": "cc3b137acb551723284a767c48d65f00",
+    "url": "/koriphey/static/media/DroidSans-Bold.cc3b137a.eot"
+  },
+  {
+    "revision": "146cd98f8ea9f2b6ea4a835657112bfd",
+    "url": "/koriphey/static/media/DroidSans.146cd98f.woff"
+  },
+  {
+    "revision": "981eef404d46b3a2d7a262e916072249",
+    "url": "/koriphey/static/media/DroidSans.981eef40.ttf"
+  },
+  {
+    "revision": "aef4495e061bfb9fc588a9cf968017f0",
+    "url": "/koriphey/static/media/DroidSans.aef4495e.eot"
+  }
+]);
