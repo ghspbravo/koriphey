c91753fc6ee6aa75c8ab28a67e337f963b753b75
ghspbravo <gmspbravo@gmail.com>
2019-07-30 19:20:39 +0500

    add resourses in footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,19 @@ import { socialVkontakte } from '../socials/socials';
 export default function Footer() {
   return (
     <div className="container mt-3">
-      <hr/>
+      <hr />
+      <div className="mb-1">
+        <span>Наши ресурсы: </span>
+        <a href="http://www.koriphey.ru/" target="_blank" rel="noopener noreferrer">
+          Сайт Гимназии</a>, <a href="http://bib.koriphey.ru/" target="_blank" rel="noopener noreferrer">
+          Библиотека</a>, <a href="http://plus.koriphey.ru/" target="_blank" rel="noopener noreferrer">
+          Интеллект-школа</a>, <a href="http://fund.koriphey.ru/" target="_blank" rel="noopener noreferrer">
+          Эндаумент</a>, <a href="http://bonus.koriphey.ru/" target="_blank" rel="noopener noreferrer">
+          Дисконтная система</a>, <a href="http://leaders.koriphey.ru/" target="_blank" rel="noopener noreferrer">
+          Школа вожатых</a>, <a href="http://mbl.mba/" target="_blank" rel="noopener noreferrer">
+          Молодежная бизнес лига</a>
+      </div>
+
       <div className="mb-1">
         <span>Мы в соц. сетях:</span>
       </div>
@@ -12,6 +24,13 @@ export default function Footer() {
         <div className="mr-1">
           {socialVkontakte('https://vk.com/koriphey_klub_vypusknikov')}
         </div>
+      </div>
+
+      <div className="mt-1">
+        <p className='small'>
+          Сервис Клуба выпускников Гимназии “Корифей”  размещен на
+          платформе <a href="https://vsvoi.ru" target='_blank' rel="noopener noreferrer">VСВОИ</a>
+        </p>
       </div>
     </div>
   )
