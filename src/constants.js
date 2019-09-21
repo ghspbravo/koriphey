8e15d98cb4aca22f52d906a29085d6ad3ee06546
ghspbravo <gmspbravo@gmail.com>
2019-09-21 10:14:08 +0500

    fixed production build

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,13 @@
+const isDevelop = process.env.NODE_ENV === "development"
+
 export const PROFILES = {
   KORIPHEY: "KORIPHEY",
   DLA8: "DLA8"
 };
 
 export const API = {
-  KORIPHEY: process.env.REACT_APP_API || "https://koriphey.us-east-2.elasticbeanstalk.com/api/v1/",
-  DLA8: process.env.REACT_APP_API || "http://dla8.us-east-2.elasticbeanstalk.com/api/v1/",
+  KORIPHEY: (isDevelop && process.env.REACT_APP_API) || "https://koriphey.us-east-2.elasticbeanstalk.com/api/v1/",
+  DLA8: (isDevelop && process.env.REACT_APP_API) || "http://dla8.us-east-2.elasticbeanstalk.com/api/v1/",
 };
 
 export const FAVICONS = {
@@ -38,11 +40,11 @@ export let isDla8 = false;
 
 
 const currentBase = window.location.hostname;
-if (/koriphey/.test(currentBase) || process.env.REACT_APP_PROJECT === PROFILES.KORIPHEY) {
+if (/koriphey/.test(currentBase) || (isDevelop && process.env.REACT_APP_PROJECT === PROFILES.KORIPHEY)) {
   CURRENT_PROFILE = PROFILES.KORIPHEY;
   isKoriphey = true;
 }
-if (/dla8/.test(currentBase) || process.env.REACT_APP_PROJECT === PROFILES.DLA8) {
+if (/dla8/.test(currentBase) || (isDevelop && process.env.REACT_APP_PROJECT === PROFILES.DLA8)) {
   CURRENT_PROFILE = PROFILES.DLA8;
   isDla8 = true;
 }
